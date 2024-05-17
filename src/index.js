de84b1adf4af690fb02b82ec5a960574f172261b
davidgr11 <davidygonzalez@hotmail.com>
2024-05-17 15:29:50 -0600

    index port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const app = require('./app')
 //const functions = require("firebase-functions");
 //const logger = require("firebase-functions/logger");
 
-app.listen(4000)
+const port = process.env.PORT || 4000;
+
+app.listen(port);
 //console.log('Server is running on port 4000');
 //exports.api = functions.https.onRequest(app)
