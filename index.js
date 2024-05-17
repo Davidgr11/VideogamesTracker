95353da6fd9f70ed03448d245d5ec3daf9db17ae
davidgr11 <davidygonzalez@hotmail.com>
2024-05-17 15:39:58 -0600

    vercerl json and package json changes

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./src/app')
+//const functions = require("firebase-functions");
+//const logger = require("firebase-functions/logger");
+
+const port = process.env.PORT || 4000;
+
+app.listen(port);
+//console.log('Server is running on port 4000');
+//exports.api = functions.https.onRequest(app)
