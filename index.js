4f81e91265973e8595594d3919bcb2a15ea4c1c8
davidgr11 <davidygonzalez@hotmail.com>
2024-05-19 19:48:11 -0600

    load images and return to last structure

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app')
-//const functions = require("firebase-functions");
-//const logger = require("firebase-functions/logger");
-
-const port = process.env.PORT || 4000;
-
-app.listen(port);
-//console.log('Server is running on port 4000');
-//exports.api = functions.https.onRequest(app)
